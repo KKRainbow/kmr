22603d6b264a44295651fdcfbf117f0461650d61
sunsijie <sunsijie@buaa.edu.cn>
2017-07-26 10:20:31 +0800

    worker framework

---
+++ kmr/master.go
@@ -0,0 +1,17 @@
+package kmr
+
+//RequestJob give executor a map or reduce job
+func RequestJob() interface{} {
+
+}
+
+//ReportStatus worker report status
+func ReportStatus() {
+
+}
+
+func Run(task) {
+	//Run server, and read from scheduler
+
+	//Generate task for one map or reduce task
+}
