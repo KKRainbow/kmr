e0c43eab8d57034d241970ff3f3745b59a70c022
sunsijie <sunsijie@buaa.edu.cn>
2017-08-03 14:51:50 +0800

    count music pmi

--- examples/musicpmi/main.go
+++ examples/musicpmi/main.go
@@ -8,22 +8,21 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/naturali/kmr/executor"
 	"github.com/naturali/kmr/mapred"
 	"github.com/naturali/kmr/pkg/aca"
-	"github.com/naturali/kmr/scheduler"
+	graph "github.com/naturali/kmr/job"
 )
 
 var musicMatcher, artistMatcher *aca.AhoCorasickMatcher
 var musicArtistMap = make(map[string][]string)
-var job = scheduler.JobGraph{}
+var job = graph.JobGraph{}
 
 func Init() {
 	musicMatcher = aca.NewAhoCorasickMatcher()
 	artistMatcher = aca.NewAhoCorasickMatcher()
 	var musicListFileDir string
 	if name, err := os.Hostname(); err == nil && (name == "arch-sunsijie-linux") {
-		musicListFileDir = "/home/sunsijie/Project/go/src/github.com/naturali/kmr/output2/"
+		musicListFileDir = "/home/sunsijie/kmr-jobs/output2"
 	} else {
 		musicListFileDir = "/mnt/cephfs"
 	}
@@ -35,60 +34,14 @@ func Init() {
 	reader := bufio.NewReader(file)
 	musics := make([]string, 0)
 	artists := make([]string, 0)
+	count := 0
 	for {
+		count++
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			break
 		}
-		splited := strings.Split(line, ",")
-		if len(splited) != 2 {
-			panic(fmt.Sprintln("Music csv malformated with line", line))
-		}
-		music := splited[0]
-		artist := splited[1]
-		if utf8.RuneCountInString(music) > 0 {
-			musics = append(musics, music)
-			musicArtistMap[music] = append(musicArtistMap[music], artist)
-		}
-		if utf8.RuneCountInString(artist) > 0 {
-			artists = append(artists, artist)
-		} else {
-			artist = "Unknown"
-		}
-	}
-	musicMatcher.Build(musics)
-	artistMatcher.Build(artists)
-}
-func main() {
-	Init()
-	cw := &executor.ComputeWrapClass{}
-	cw.BindMapper(wcmap)
-	cw.BindReducer(wcreduce)
-	cw.Run()
-}
-
-func Init() {
-	musicMatcher = aca.NewAhoCorasickMatcher()
-	artistMatcher = aca.NewAhoCorasickMatcher()
-	var musicListFileDir string
-	if name, err := os.Hostname(); err == nil && (name == "arch-sunsijie-linux") {
-		musicListFileDir = "/home/sunsijie/Project/go/src/github.com/naturali/kmr/output2/"
-	} else {
-		musicListFileDir = "/mnt/cephfs"
-	}
-	file, err := os.Open(filepath.Join(musicListFileDir, ".", "filtered_music"))
-	defer file.Close()
-	if err != nil {
-		panic(err)
-	}
-	reader := bufio.NewReader(file)
-	musics := make([]string, 0)
-	artists := make([]string, 0)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
+		line = strings.Replace(line, " ", "", -1)
 		splited := strings.Split(line, ",")
 		if len(splited) != 2 {
 			panic(fmt.Sprintln("Music csv malformated with line", line))
@@ -105,35 +58,36 @@ func Init() {
 			artist = "Unknow"
 		}
 	}
+	fmt.Println("total: ", count)
 	musicMatcher.Build(musics)
 	artistMatcher.Build(artists)
 }
 
 func CountMusicMap(key interface{}, value interface{}, output func(k, v interface{}), reporter interface{}) {
-	line := strings.Replace(string(key.(string)), " ", "", -1)
-	strs, idxs := musicMatcher.Match(line)
+	line := strings.Replace(string(value.(string)), " ", "", -1)
+	strs, _ := musicMatcher.Match(line)
 	for _, m := range strs {
-		output(m, 1)
+		output(m, uint32(1))
 	}
 }
 
 func CountArtistMap(key interface{}, value interface{}, output func(k, v interface{}), reporter interface{}) {
-	line := strings.Replace(string(key.(string)), " ", "", -1)
-	strs, idxs := artistMatcher.Match(line)
+	line := strings.Replace(string(value.(string)), " ", "", -1)
+	strs, _ := artistMatcher.Match(line)
 	for _, m := range strs {
-		output(m, 1)
+		output(m, uint32(1))
 	}
 }
 
 func CountPairMap(key interface{}, value interface{}, output func(k, v interface{}), reporter interface{}) {
-	line := strings.Replace(string(key.(string)), " ", "", -1)
+	line := strings.Replace(string(value.(string)), " ", "", -1)
 	musics, _ := artistMatcher.Match(line)
 	artists, _ := musicMatcher.Match(line)
 	for _, m := range musics {
 		for _, artist := range artists {
 			for _, artistOfMusic := range musicArtistMap[m] {
 				if artist == artistOfMusic {
-					output(strings.Join([]string{m, artist}, ","), 1)
+					output(strings.Join([]string{m, artist}, ","), uint32(1))
 				}
 			}
 		}
@@ -141,7 +95,7 @@ func CountPairMap(key interface{}, value interface{}, output func(k, v interface
 }
 
 func CountAllMap(key interface{}, value interface{}, output func(k, v interface{}), reporter interface{}) {
-	output("All", 1)
+	output("All", uint32(1))
 }
 
 func AggregateReducer(key interface{}, valuesNext mapred.ValueIterator, output func(v interface{}), reporter interface{}) {
@@ -156,29 +110,38 @@ func PMIMapper(key interface{}, value interface{}, output func(k, v interface{})
 }
 
 func PMIReducer(key interface{}, valuesNext mapred.ValueIterator, output func(v interface{}), reporter interface{}) {
-	cmm := job.GetOutputOf("CMM")
 }
 
 func main() {
 	job.Name = "ABC"
-	countMusicMapper := mapred.GetFunctionMapper(CountMusicMap, mapred.String, mapred.Bytes, mapred.String, mapred.Uint32, func() {})
-	countArtistMapper := mapred.GetFunctionMapper(CountArtistMap, mapred.String, mapred.Bytes, mapred.String, mapred.Uint32, func() {})
-	countPairMapper := mapred.GetFunctionMapper(CountPairMap, mapred.String, mapred.Bytes, mapred.String, mapred.Uint32, func() {})
-	countAllMapper := mapred.GetFunctionMapper(CountAllMap, mapred.String, mapred.Bytes, mapred.String, mapred.Uint32, func() {})
-	pmiMapper := mapred.GetFunctionMapper(PMIMapper, mapred.String, mapred.Bytes, mapred.String, mapred.Bytes, func() {})
+	Init()
+	countMusicMapper := mapred.GetFunctionMapper(CountMusicMap, mapred.String, mapred.String, mapred.String, mapred.Uint32, func() {})
+	countArtistMapper := mapred.GetFunctionMapper(CountArtistMap, mapred.String, mapred.String, mapred.String, mapred.Uint32, func() {})
+	countPairMapper := mapred.GetFunctionMapper(CountPairMap, mapred.String, mapred.String, mapred.String, mapred.Uint32, func() {})
+	countAllMapper := mapred.GetFunctionMapper(CountAllMap, mapred.String, mapred.String, mapred.String, mapred.Uint32, func() {})
+	pmiMapper := mapred.GetFunctionMapper(PMIMapper, mapred.String, mapred.String, mapred.String, mapred.Bytes, func() {})
 
 	aggregateReducer := mapred.GetFunctionReducer(AggregateReducer, mapred.Bytes, mapred.Uint32, mapred.Bytes, mapred.Uint32, func() {})
-	pmiReducer := mapred.GetFunctionMapper(PMIReducer, mapred.String, mapred.Bytes, mapred.String, mapred.String, func() {})
+	pmiReducer := mapred.GetFunctionReducer(PMIReducer, mapred.String, mapred.Bytes, mapred.String, mapred.String, func() {})
 
-	inputSentencesFiles := make([]string, 10)
-	inputMusicListFiles := make([]string, 10)
+	inputSentencesFiles := &graph.InputFiles{
+		[]string{"a.t"},
+		"textstream",
+	}
 
-	cmj := job.AddMapper(countMusicMapper, inputSentencesFiles).AddReducer(aggregateReducer, 10).SetName("CM")
-	caj := job.AddMapper(countArtistMapper, inputSentencesFiles).AddReducer(aggregateReducer, 10).SetName("CA")
-	cpj := job.AddMapper(countPairMapper, inputSentencesFiles).AddReducer(aggregateReducer, 10).SetName("CP")
-	callj := job.AddMapper(CountAllMap, inputSentencesFiles).AddReducer(aggregateReducer, 10).SetName("CALL")
+	inputMusicListFiles := &graph.InputFiles{
+		[]string{"a.t"},
+		"textstream",
+	}
 
-	pmij := job.AddMapper(PMIMapper, inputMusicListFiles).AddReducer(PMIReducer, 10).DenpendOn(cmj, caj, cpj, caj).SetName("")
+	callj := job.AddMapper(countAllMapper, inputSentencesFiles).AddReducer(aggregateReducer, 1).AddReducer(aggregateReducer, 1).SetName("CALL")
+	caj := job.AddMapper(countArtistMapper, inputSentencesFiles).AddReducer(aggregateReducer, 1).SetName("CA")
+	cpj := job.AddMapper(countPairMapper, inputSentencesFiles).AddReducer(aggregateReducer, 1).SetName("CP")
+	cmj := job.AddMapper(countMusicMapper, inputSentencesFiles).AddReducer(aggregateReducer, 1).SetName("CM")
+
+	pmij := job.AddMapper(pmiMapper, inputMusicListFiles).AddReducer(pmiReducer, 10).DependOn(cmj, caj, cpj, callj).SetName("pmij")
+
 
 	job.Run()
+	fmt.Println(countAllMapper, pmiMapper, pmiReducer, callj, pmij)
 }
